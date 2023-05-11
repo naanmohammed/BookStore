841b0a114c144a1a7755c6d5e7064af15a72c3b4
Naan Ahmed Mohammed <naanahmed24@gmail.com>
2023-05-11 20:46:11 +0300

    Add books container component

---
+++ src/components/BooksCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+
+function BookCard({ book }) {
+  const renderBooks = book.map((book) => (
+    <div className="bookContainer" key={book.id}>
+      <div className="bookTitle">
+        <p>{book.category}</p>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <ul className="buttonsList">
+          <li><button type="button">Comments</button></li>
+          <li><button type="button">Remove</button></li>
+          <li><button type="button">Edit</button></li>
+        </ul>
+      </div>
+      <div className="bookChart">
+        <div>Chart</div>
+        <div>
+          <p>{book.progres}</p>
+          <p>COMPLETED</p>
+        </div>
+      </div>
+      <div className="bookChapter">
+        <p>{book.currentChapter}</p>
+        <p>{book.chapterNumber}</p>
+        <button type="button">Update Progress</button>
+      </div>
+    </div>
+  ));
+  return (
+    <div className="mainContainer">
+      {renderBooks}
+    </div>
+  );
+}
+
+BookCard.propTypes = {
+  book: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
+};
+
+export default BookCard;
