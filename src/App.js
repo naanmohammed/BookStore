63467cb105cf5df1cdb59ef043e03ea90449125b
naanmohammed <naanahmed24@gmail.com>
2023-05-12 10:19:36 +0300

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Books from './components/books';
+import Books from './components/Books';
 import Navbar from './components/navBar';
-import Categories from './components/categories';
+import Categories from './components/Categories';
 
 function App() {
   return (
