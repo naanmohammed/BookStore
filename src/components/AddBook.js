1b00fdcf5c27b0a072c01bd04eda948bdd5a24bd
naanmohammed <naanahmed24@gmail.com>
2023-05-15 20:36:33 +0300

    Update addbook function and implement it

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
 function AddBook() {
-  return (
-    <div className="addBook">
-      <hr />
-      <h3>ADD NEW BOOK</h3>
-      <form className="addBookForm">
-        <input type="text" placeholder="Book title" />
-        <select key="author">
-          <option value="Authors">Authors</option>
-        </select>
-        <button type="button">Add Book</button>
-      </form>
+  const [values, setValue] = useState({});
+  const dispatch = useDispatch();
 
-    </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValue((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+      item_id: crypto.randomUUID(),
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook(values));
+    setValue({});
+  };
+
+  return (
+    <section className="addBook">
+      <h2>Add new book</h2>
+      <form className="addBookForm">
+        <input
+          value={values.title || ''}
+          type="text"
+          name="title"
+          placeholder="Book title"
+          onChange={handleChange}
+        />
+
+        <input
+          value={values.author || ''}
+          type="text"
+          name="author"
+          placeholder="Author"
+          onChange={handleChange}
+        />
+
+        <button type="submit" onClick={handleSubmit}>Add Book</button>
+      </form>
+    </section>
   );
 }
 
